c6ac7c531bf767db54e77f40591fc74eda641b8e
John Leamy <jwleamy@gmail.com>
2023-04-19 13:51:12 -0700

    d

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-require ('dotenv').config();
 console.log("process",process.env.MONGODB_URI);
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/',
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
