468280216abca67999b8d1b3fcd453b9e481ca67
John Leamy <jwleamy@gmail.com>
2023-04-19 16:12:11 -0700

    s

--- client/src/componentsAdmin/adminNav.js
+++ client/src/componentsAdmin/adminNav.js
@@ -1,5 +1,6 @@
 import '../styles/output.css';
 import Auth from "../../src/utils/auth";
+import { Link } from 'react-router-dom';
 
 function AdminNav () {
     const logout = (event) => {
@@ -9,25 +10,33 @@ function AdminNav () {
     return (
         <nav className="bg-mainWhite mx-auto flex items-center justify-between p-6 lg:px-8">
             <div className="flex lg:flex-1">
-                <a href="AdminHome" className="-m-1.5 p-1.5">
-                <span className="sr-only">GoBommarito</span>
-                <img className="h-12 w-auto" src='../Logo.png' alt="GoBommarito" />
-                </a>
+                <Link to='/AdminHome'>
+                    <h2 className="-m-1.5 p-1.5">
+                    <span className="sr-only">GoBommarito</span>
+                    <img className="h-12 w-auto" src='../Logo.png' alt="GoBommarito" />
+                    </h2>
+                </Link>
             </div>
             <div className="p-4">
-                <a href="AdminHome" className="p-4 text-lg rounded-md hover:bg-mainBlue hover:text-mainWhite font-semibold leading-6 text-gray-900">
-                    Home
-                </a>
-                <a href="Posts" className="p-4 text-lg  rounded-md font-semibold hover:bg-mainBlue hover:text-mainWhite leading-6 text-gray-900">
-                    Posts
-                </a>
-                <a href="Categories" className="p-4 rounded-md text-lg font-semibold hover:bg-mainBlue hover:text-mainWhite leading-6 text-gray-900">
-                    Categories</a>
+                <Link to='/AdminHome'>
+                    <h2 className="p-4 text-lg rounded-md hover:bg-mainBlue hover:text-mainWhite font-semibold leading-6 text-gray-900">
+                        Home
+                    </h2>
+                </Link>
+                <Link to='/Posts'>
+                    <h2 className="p-4 text-lg  rounded-md font-semibold hover:bg-mainBlue hover:text-mainWhite leading-6 text-gray-900">
+                        Posts
+                    </h2>
+                </Link>
+                <Link to='/Categories'>
+                    <h2 className="p-4 rounded-md text-lg font-semibold hover:bg-mainBlue hover:text-mainWhite leading-6 text-gray-900">
+                        Categories</h2>
+                </Link>
             </div>
             <div className="hidden lg:flex lg:flex-1 lg:justify-end" onClick={logout}>
-                <a href="/" className="p-4 rounded-md text-lg font-semibold leading-6 text-gray-900 hover:bg-mainBlue hover:text-mainWhite">
+                <h2 href="/" className="p-4 rounded-md text-lg font-semibold leading-6 text-gray-900 hover:bg-mainBlue hover:text-mainWhite">
                 Log out <span aria-hidden="true">&rarr;</span>
-                </a>
+                </h2>
             </div>
         </nav>
     )
